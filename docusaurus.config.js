9d7b0b7fb6a7665bbaa454d1ca8211dd7e5087fa
angordeyev <angordeyev@gmail.com>
2023-07-26 19:53:30 +0300

    Fix search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
         indexName: 'bugbuns',
 
         // Optional: see doc section below
-        // contextualSearch: true,
+        contextualSearch: false,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
